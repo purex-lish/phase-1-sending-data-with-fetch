420e74eeeff35bb1a3f0811783cd637a546cffb5
Purity Wambua <puritywamb@gmail.com>
2024-07-13 22:46:09 +0300

    send data with fetch

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Add your code here
+function submitData(name, email) {
+    let formData = {
+      name: name,
+      email: email
+    };
+  
+    let configObject = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify(formData)
+    };
+  
+    return fetch("http://localhost:3000/users", configObject)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.json();
+      })
+      .then(data => {
+        // Assuming the server responds with an object containing the new id
+        document.body.innerHTML = data.id;
+      })
+      .catch(error => {
+        document.body.innerHTML = error.message;
+      });
+  }
+  
+  module.exports = { submitData };
